1effe70209b7fbfbfd1757af1a4a6b8c3992d7d6
JonEast87 <j.d.east@icloud.com>
2024-09-04 21:17:28 -0700

    added player 'ship', can rotate it now. will add player movement soon.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
 	pygame.init()
@@ -10,13 +11,20 @@ def main():
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
-	
+	x = SCREEN_WIDTH / 2
+	y = SCREEN_HEIGHT / 2
+
+	player = Player(x, y)
+
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		
+		player.update(dt)
+
 		pygame.Surface.fill(screen, (0, 0, 0))
+		player.draw(screen)
 		pygame.display.flip()
 
 		# limit frames to 60fps
