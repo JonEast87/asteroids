a07880e7f155ca72e7d5195afc417120d7dcacc3
JonEast87 <j.d.east@icloud.com>
2024-09-05 17:44:57 -0700

    swapped up main.py to utilize groups for updating player status or drawing

---
+++ asteroid.py
@@ -0,0 +1,4 @@
+import pygame
+
+from circleshape import CircleShape
+
--- main.py
+++ main.py
@@ -6,25 +6,33 @@ def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	clock = pygame.time.Clock()
-	dt = 0
 
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
-	x = SCREEN_WIDTH / 2
-	y = SCREEN_HEIGHT / 2
 
-	player = Player(x, y)
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+
+	Player.containers = (updatable, drawable)
+
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+	dt = 0
 
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		
-		player.update(dt)
+		for player in updatable:
+			player.update(dt)
 
-		pygame.Surface.fill(screen, (0, 0, 0))
-		player.draw(screen)
+		screen.fill("black")
+
+		for player in drawable:
+			player.draw(screen)
+		
 		pygame.display.flip()
 
 		# limit frames to 60fps
